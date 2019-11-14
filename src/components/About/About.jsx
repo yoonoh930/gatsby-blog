7cab6c5b9a0b3f452f67654d06b3bdeb212743f3
Yoon Oh <yoonoh930@gmail.com>
2019-11-14 15:25:38 +0100

    Added other posts

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,9 +5,9 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <h1>
-          Edit About component or pages/about.jsx to include your information.
-        </h1>
+        <h2>
+        This page is still on construction. Check later.
+        </h2>
       </div>
     );
   }
